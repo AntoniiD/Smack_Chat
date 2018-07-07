cb662373c2417eae6e708db185a3ff8cd1c20537
Antonii_D <daraevanton@gmail.com>
2018-07-06 23:06:42 -0700

    Login Screen

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Anton on 7/6/18.
+//  Copyright © 2018 Antonii D. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Anton on 7/6/18.
+//  Copyright © 2018 Antonii D. All rights reserved.
+//
+
+import Foundation
+
+
+// Segues
+let TO_LOGIN = "toLogin"
